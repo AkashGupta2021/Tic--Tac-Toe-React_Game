fdbf36265fad2eaac5a6d96ff628f7950731a401
Akash Kumar Gupta <80001249+AkashGupta2021@users.noreply.github.com>
2023-05-19 00:38:41 +0530

    Setup gh-pages

---
+++ src/Square.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Square (props) {
+
+  return (
+    <div className="square" onClick={props.onClick}>
+        <h5>{props.value}</h5>
+    </div>
+  )
+}
